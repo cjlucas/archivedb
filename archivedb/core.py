676533130644f97509c1687d116bcb9d2b4418a1
Chris Lucas <cjlucas07@gmail.com>
2011-08-13 00:41:51 -0700

    added enum checks

--- archivedb/core.py
+++ archivedb/core.py
@@ -6,7 +6,8 @@ log = archivedb.logger.get_logger("logs", "archivedb.log")
 import archivedb.config as config
 import archivedb.threads
 import archivedb.sql
-from archivedb.common import split_path
+import archivedb.monitor
+#from archivedb.common import split_path
 
 def main():
 	args = config.args
@@ -17,12 +18,10 @@ def main():
 	db_pass = config.args["db_pass"]
 	db_name = config.args["db_name"]
 	
-	#blah = split_path(args["watch_dirs"], "/mnt/user/stuff/sabnzbd/complete/The.Colbert.Report.2011.08.11.Gloria.Steinem.720p.HDTV.x264-LMAO/the.colbert.report.2011.08.11.gloria.steinem.720p.hdtv.x264-lmao.mkv")
-	#print(blah)
-	
 	db = archivedb.sql.create_conn(db_host, db_user, db_pass, db_name, db_port)	
+	
 	# check if tables exist
-	# add code to append to tables if plugins are enabled
+	# add code to append to tables list if plugins are enabled
 	tables = ["archive"]
 	c = db.cursor()
 	for t in tables:
@@ -32,7 +31,22 @@ def main():
 			log.info("required table '{0}' doesn't exist, creating.".format(t))
 			archivedb.sql.create_table(c, args["tables"][t])
 			sys.exit(1)
-	
+			
+	# perform enum checks here
+	# check if watch dirs in conf file match watch dirs set in database
+	# use set() because it doesn't care about order
+	conf_watch_dirs = set(args["watch_dirs"])
+	db_watch_dirs	= set(archivedb.sql.get_enum(c, "archive"))
+	if conf_watch_dirs == db_watch_dirs:
+		log.debug("watch_dirs in conf match watch dirs in database, no need to update enum")
+
+	else:
+		log.debug("watch_dirs in conf don't match to watch dirs in database, need to update")
+		log.debug("conf_watch_dirs	= {0}".format(conf_watch_dirs))
+		log.debug("db_watch_dirs	= {0}".format(db_watch_dirs))
+		archivedb.sql.alter_enum(c, "archive", "watch_dir", args["watch_dirs"])
+		
+		
 	## create threads ##
 	threads = ["inotify", "oswalk"]
 	threads_dict = archivedb.threads.initialize_child_processes(threads)
