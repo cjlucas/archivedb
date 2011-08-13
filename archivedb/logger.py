b01129317a903b17b9e05b94b933e6f9252e74b8
Chris Lucas <cjlucas07@gmail.com>
2011-08-12 19:27:38 -0700

    cleaner output for console logging

--- archivedb/logger.py
+++ archivedb/logger.py
@@ -3,13 +3,14 @@ import os, sys, logging, logging.handlers
 def get_logger(log_path, log_file):
 	#LOG_PATH = os.path.join(os.path.expanduser(os.path.split(sys.argv[0])[0]), "logs")
 	#LOG_FILE = "archivedb.log"
-	LOG_FORMAT = "%(asctime)s :: %(levelname)s :: %(filename)s:%(lineno)s :: %(funcName)s :: %(message)s"
+	LOG_FORMAT 		= "%(asctime)s :: %(levelname)-8s :: %(filename)s:%(lineno)s :: %(funcName)s :: %(message)s"
+	CONSOLE_FORMAT	= "%(asctime)s :: %(levelname)-8s :: %(message)s"
 	# create logs directory if doesn't exist
 	if not os.path.isdir(log_path):
 		os.mkdir(log_path)
 
-	formatter = logging.Formatter(LOG_FORMAT)
-
+	log_formatter		= logging.Formatter(LOG_FORMAT)
+	console_formatter	= logging.Formatter(CONSOLE_FORMAT)	
 	log = logging.getLogger("archivedb")
 	log.setLevel(logging.DEBUG)
 
@@ -21,10 +22,10 @@ def get_logger(log_path, log_file):
 	console = logging.StreamHandler()
 	
 	rotator.setLevel(logging.DEBUG)
-	rotator.setFormatter(formatter)
+	rotator.setFormatter(log_formatter)
 
 	console.setLevel(logging.INFO)
-	console.setFormatter(formatter)
+	console.setFormatter(console_formatter)
 	
 	log.addHandler(rotator)
 	log.addHandler(console)
