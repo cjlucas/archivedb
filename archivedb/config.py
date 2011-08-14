b3df3b6c530cf2f2e521c606d537a8bb92f204d9
Chris Lucas <cjlucas07@gmail.com>
2011-08-14 01:00:50 -0700

    disabled inotify until it's actually implemented

--- archivedb/config.py
+++ archivedb/config.py
@@ -194,6 +194,9 @@ if __name__ == 'archivedb.config':
 	config = validate_config(CONF_FILE)
 	args = get_args()
 	
+	# disable inotify until it's actually implemented
+	del args["threads"][args["threads"].index("inotify")]
+	
 	## static args here ##
 	args["threads"] = ["inotify", "oswalk"]
 	
