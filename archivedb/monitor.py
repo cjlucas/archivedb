1d541c6cb19e2edf080a8bf3311102709120f5d8
Chris Lucas <cjlucas07@gmail.com>
2011-08-17 16:29:42 -0700

    compartmentalized a lot of code, initial code for IN_CLOSE_WRITE support added

--- archivedb/monitor.py
+++ archivedb/monitor.py
@@ -6,17 +6,61 @@ from archivedb.common import md5sum, split_path, escape_quotes
 log = logging.getLogger(__name__)
 args = config.args
 
+class EmptyClass:
+	def __init__(self):
+		pass
+
 if os.name == 'posix': # linux only
 	try:
 		import pyinotify
-		from pyinotify import IN_CLOSE_WRITE, IN_DELETE, IN_MOVED_FROM, IN_MOVED_TO, IN_ISDIR, IN_CREATE
+		from pyinotify import ProcessEvent, IN_CLOSE_WRITE, IN_DELETE, IN_MOVED_FROM, IN_MOVED_TO, IN_ISDIR, IN_CREATE
 	except ImportError:
 		log.warning("module 'pyinotify' not found. disabling inotify monitoring")
 		del args["threads"][args["threads"].index("inotify")]
+		# fix for class initialization below
+		ProcessEvent = EmptyClass
 
 
 # disable oswalk thread for testing
-del args["threads"][args["threads"].index("oswalk")]		
+#del args["threads"][args["threads"].index("oswalk")]
+
+def is_ignored_file(f):
+	for regex in args["ignore_files"]:
+		if re.search(regex, f, re.I):
+			log.info("file '{0}' matched '{1}', skipping.".format(f, regex))
+			return(True)
+	
+	return(False)
+	
+def is_ignored_directory(full_path):
+	for d in args["ignore_dirs"]:
+		if d in full_path:
+			log.info("directory '{0}' matched ignore_dir '{1}', skipping".format(full_path, d))
+			return(True)
+	
+	return(False)
+	
+def process_file(db, full_path):
+	mtime			= os.stat(full_path).st_mtime
+	size			= os.stat(full_path).st_size
+	
+	(watch_dir, path, filename) = split_path(args["watch_dirs"], full_path)
+	
+	data = db.get_fields(watch_dir, path, filename, ["mtime","size"])
+	
+	if not data: # file is new
+		db.insert_file(watch_dir, path, filename, md5sum(full_path), mtime, size)
+	else:
+		old_mtime	= data[0][0]
+		old_size	= data[0][1]
+		log.debug("old_mtime = {0}".format(old_mtime))
+		log.debug("mtime = {0}".format(mtime))
+		log.debug("old_size = {0}".format(old_size))
+		log.debug("size = {0}".format(size))
+		# check if it has changed
+		if int(old_mtime) != int(mtime) or int(old_size) != int(size):
+			rows_changed = db.update_file(watch_dir, path, filename, md5sum(full_path), mtime, size)
+			log.debug("rows_changed = {0}".format(rows_changed))
 
 def run_oswalk():
 	log.debug("start run_oswalk")
@@ -39,42 +83,15 @@ def run_oswalk():
 			
 			for root, dirs, files in os.walk(watch_dir):
 				for f in files:
-					# check if f should be ignored
-					skip = False
-					for regex in args["ignore_files"]:
-						if re.search(regex, f, re.I):
-							log.info("file '{0}' matched '{1}', skipping.".format(f, regex))
-							skip = True
-							break
-					
-					if skip:
-						continue
-					full_path		= os.path.join(root, f)
-					mtime			= os.stat(full_path).st_mtime
-					size			= os.stat(full_path).st_size
-					
-					(watch_dir, path, filename) = split_path(args["watch_dirs"], full_path)
-					
-					data = db.get_fields(watch_dir, path, filename, ["mtime","size"])
-					
-					if not data: # file is new
-						db.insert_file(watch_dir, path, filename, md5sum(full_path), mtime, size)
-					else:
-						old_mtime	= data[0][0]
-						old_size	= data[0][1]
-						log.debug("old_mtime = {0}".format(old_mtime))
-						log.debug("mtime = {0}".format(mtime))
-						log.debug("old_size = {0}".format(old_size))
-						log.debug("size = {0}".format(size))
-						# check if it has changed
-						if int(old_mtime) != int(mtime) or int(old_size) != int(size):
-							rows_changed = db.update_file(watch_dir, path, filename, md5sum(full_path), mtime, size)
-							log.debug("rows_changed = {0}".format(rows_changed))
+					full_path = os.path.join(root, f)
+					if not is_ignored_file(f) and not is_ignored_directory(full_path):
+						process_file(db, full_path)
 					
 			# sleep for an hour, figure out a way to make this more customizable
+			log.debug("sleeping")
 			time.sleep(3600)
 	
-class InotifyHandler(pyinotify.ProcessEvent):
+class InotifyHandler(ProcessEvent):
 	def my_init(self):
 		log.debug("calling my_init()")
 		self.db = sql.DatabaseConnection(
@@ -89,7 +106,9 @@ class InotifyHandler(pyinotify.ProcessEvent):
 	def process_IN_CLOSE_WRITE(self, event):
 		log.debug(event)
 		full_path = event.pathname
-		#
+		f = event.e
+		if not is_ignored_file(f) and not is_ignored_directory(full_path):
+			process_file(self.db, full_path)
 
 	
 	def process_IN_DELETE(self, event):
