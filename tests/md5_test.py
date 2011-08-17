3ee2286c734d0f8e8c5790258cf9ef3277125129
Chris Lucas <cjlucas07@gmail.com>
2011-08-16 22:12:51 -0700

    added code to check if file has been moved/deleted while generating md5

--- tests/md5_test.py
+++ tests/md5_test.py
@@ -1,21 +1,23 @@
-import sys, time, hashlib
+import os, sys, time, hashlib
 
 def md5sum(f, block_size=2**20):
 	md5 = hashlib.md5()
-	
 	with open(f, "rb") as fp:
 		while True:
+			# check if file has moved during md5 creation
+			if not os.path.isfile(f):
+				return(None)
 			data = fp.read(block_size)
 			if not data:
 				break
 			md5.update(data)
-	
+			
 	return(md5.hexdigest())
 
 def main():
 	f = sys.argv[-1]
 	start = time.time()
-	print(f + " " + md5sum(f))
+	print(md5sum(f))
 	print("Time Elapsed: {0} seconds".format(time.time() - start))
 	
 if __name__ == '__main__':
