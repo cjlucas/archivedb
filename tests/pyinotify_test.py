65874cb114fcce95572bfe099a518ec1a26e4daf
root <root@slackware.home>
2011-08-17 01:35:24 -0700

    add few examples of how it will work

--- tests/pyinotify_test.py
+++ tests/pyinotify_test.py
@@ -2,19 +2,28 @@ import pyinotify
 from pyinotify import IN_CLOSE_WRITE, IN_DELETE, IN_MOVED_FROM, IN_MOVED_TO
 
 class InotifyHandler(pyinotify.ProcessEvent):
-	def process_default(self):
-		print(self)
+	def my_init(self):
+		print("OMG")
+
+	def process_IN_CLOSE_WRITE(self, event):
+		print("IN_CLOSE_WRITE NOW")
+		print(event.path)
+
+	def process_default(self, event):
+		print("HERE")
+		print(event)
 
 def main():
 	masks = IN_CLOSE_WRITE | IN_DELETE | IN_MOVED_FROM | IN_MOVED_TO
 
 	wm = pyinotify.WatchManager()
-	notifier = pyinotify.Notifier(wm, default_proc_fun=InotifyHandler)
-	
-	wm.add_watch("/mnt/user/stuff/test", masks, rec=True, auto_add=True)
+	notifier = pyinotify.Notifier(wm, default_proc_fun=InotifyHandler())
+	#notifier = pyinotify.Notifier(wm)
+	wm.add_watch("/mnt/user/stuff/test", pyinotify.ALL_EVENTS, rec=True, auto_add=True)
 	
 	notifier.loop()
 
 
 if __name__ == '__main__':
+	print("start")
 	main()
