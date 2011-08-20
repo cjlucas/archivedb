0fbb5691d25d22f365779161bcff17b24c5349a9
Chris Lucas <cjlucas07@gmail.com>
2011-08-19 20:47:05 -0700

    explicitly defined event paths as directories by adding trailing os.sep (so split_path() handles it properly)

--- archivedb/monitor.py
+++ archivedb/monitor.py
@@ -177,8 +177,8 @@ class InotifyHandler(ProcessEvent):
 			src_full_path = None
 			
 		if src_full_path:
-			src_split_path	= split_path(args["watch_dirs"], src_full_path)
-			dest_split_path	= split_path(args["watch_dirs"], dest_full_path)
+			src_split_path	= split_path(args["watch_dirs"], src_full_path + os.sep)
+			dest_split_path	= split_path(args["watch_dirs"], dest_full_path + os.sep)
 			
 			if event.dir:
 				rows_changed = self.db.move_directory(src_split_path, dest_split_path)
