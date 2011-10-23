58edb9df3a41c5ebc412c75fce9bd196d001f078
Chris Lucas <cjlucas07@gmail.com>
2011-10-23 15:13:38 -0700

    converted tabs to spaces

--- archivedb/common.py
+++ archivedb/common.py
@@ -1,67 +1,70 @@
 import os, re, hashlib
 
-def md5sum(f, block_size=2**20):
-	md5 = hashlib.md5()
-	
-	# check if file was moved/deleted before opening
-	if not os.path.isfile(f):
-		return(None)
-	
-	try:
-		with open(f, "rb") as fp:
-			while True:
-				# check if file has moved/deleted during md5 creation
-				if not os.path.isfile(f):
-					return(None)
-				data = fp.read(block_size)
-				if not data:
-					break
-				md5.update(data)
-	except IOError:
-		# another failsafe in case open() throws error
-		return(None)
-	
-	return(md5.hexdigest())
-	
+def md5sum(f, block_size=2 ** 20):
+    md5 = hashlib.md5()
+
+    # check if file was moved/deleted before opening
+    if not os.path.isfile(f):
+        return(None)
+
+    try:
+        with open(f, "rb") as fp:
+            while True:
+                # check if file has moved/deleted during md5 creation
+                if not os.path.isfile(f):
+                    return(None)
+                data = fp.read(block_size)
+                if not data:
+                    break
+                md5.update(data)
+    except IOError:
+        # another failsafe in case open() throws error
+        return(None)
+
+    return(md5.hexdigest())
+
 
 def split_path(watch_dirs, p):
-	"""
-	Parses out a path to a file in a format suitable for
-	inserting into and searching the database
-	
-	Args:
-	watch_dirs - list of directories script is watching
-	p - full path to file (if p is a directory [requires trailing slash], file_name will be "")
-	
-	Returns: tuple (watch_dir, base_path, file_name)
-	"""
-	
-	watch_dir = ""
-	base_path = ""
-	file_name = ""
-	
-	# find which watch_dir is being used
-	for d in watch_dirs:
-		split = re.split("^{0}".format(d), p)
-		if split[0] == "":
-			watch_dir = d.rstrip(os.sep)
-			base_path = split[1].lstrip(os.sep)
-			break
-		
-	base_path, file_name = os.path.split(base_path)
-	
-	return(watch_dir, base_path, file_name)
-	
+    """
+    Parses out a path to a file in a format suitable for
+    inserting into and searching the database
+    
+    Args:
+    watch_dirs - list of directories script is watching
+    p - full path to file (if p is a directory [requires trailing slash], file_name will be "")
+    
+    Returns: tuple (watch_dir, base_path, file_name)
+    """
+
+    watch_dir = ""
+    base_path = ""
+    file_name = ""
+
+    # find which watch_dir is being used
+    for d in watch_dirs:
+        split = re.split("^{0}".format(d), p)
+        if split[0] == "":
+            watch_dir = d.rstrip(os.sep)
+            base_path = split[1].lstrip(os.sep)
+            break
+
+    base_path, file_name = os.path.split(base_path)
+
+    return(watch_dir, base_path, file_name)
+
+def join_path(watch_dir, path, file_name):
+    return(os.path.join(watch_dir, path, file_name))
+
 def escape_quotes(s):
-	s = s.replace("\'","\\'").replace('\"','\\"')
-	return(s)
-	
+    s = s.replace("\'", "\\'").replace('\"', '\\"')
+    return(s)
+
 
 def list_to_enum(watch_dirs):
-	out = "enum('{0}')".format("','".join(watch_dirs))
-	return(out)
+    out = "enum('{0}')".format("','".join(watch_dirs))
+    return(out)
 
 def enum_to_list(enum):
-	regex = "\'([^\']*)\'[\,\)]"
-	watch_list = re.findall(regex, enum)
-	return(watch_list)
+    regex = "\'([^\']*)\'[\,\)]"
+    watch_list = re.findall(regex, enum)
+    return(watch_list)
